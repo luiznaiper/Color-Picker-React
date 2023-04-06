37b66a0eea9878741170728ec2ec4aac2830e37a
luiznaiper <luiz_naiper@hotmail.com>
2023-04-06 10:07:58 -0300

    :heavy_plus_sign: Add values.js library for generating colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
 import ColorList from './components/ColorList'
 import Form from './components/Form'
+import Values from 'values.js'
 
 function App() {
+  const [colors, setColors] = useState([new Values('#fb8122').all(10)])
+
   return (
     <main>
       <Form />
