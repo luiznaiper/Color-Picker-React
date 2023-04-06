6d07f85288270eb2f8e6413837e20f63b46de468
luiznaiper <luiz_naiper@hotmail.com>
2023-04-06 10:42:29 -0300

    :construction: Configure react-tostify

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import ColorList from './components/ColorList'
 import Form from './components/Form'
 import Values from 'values.js'
+import { ToastContainer, toast } from 'react-toastify'
 
 function App() {
   const [colors, setColors] = useState(new Values('#fb8122').all(10))
@@ -10,6 +11,7 @@ function App() {
     <main>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   )
 }
